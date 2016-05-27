65327a5d34fffb104137e8c7bb2ecdc22b05dc01
skaxwng <samkj.ks@gmail.com>
2016-05-27 09:31:27 -0500

    Updated check-npm.js

--- check-npm.js
+++ check-npm.js
@@ -2,7 +2,7 @@ import { checkNpmVersions } from 'meteor/tmeasday:check-npm-versions';
 
 checkNpmVersions({
   'apollo-client': '^0.3.8',
-  'graphql-tools': '^0.3.14',
+  'apollo-server': '^0.1.1',
   'express': '^4.13.4',
   'http-proxy-middleware': '^0.15.0'
 }, 'my:awesome-package');
