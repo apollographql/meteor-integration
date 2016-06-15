0b78c4c91a87c24d00b374aead0a50a97ea8b9b0
Loren Sands-Ramshaw <lorensr@gmail.com>
2016-06-15 01:10:12 -0400

    Fix client-side check-npm-versions error about server packages

--- check-npm.js
+++ check-npm.js
@@ -1,8 +1,14 @@
+import { Meteor } from 'meteor/meteor';
 import { checkNpmVersions } from 'meteor/tmeasday:check-npm-versions';
 
-checkNpmVersions({
-  'apollo-client': '^0.3.12',
-  'apollo-server': '^0.1.1',
-  'express': '^4.13.4',
-  'http-proxy-middleware': '^0.15.0'
-}, 'my:awesome-package');
+if (Meteor.isClient) {
+  checkNpmVersions({
+    'apollo-client': '^0.3.12'
+  }, 'apollo');
+} else {
+  checkNpmVersions({
+    'apollo-server': '^0.1.1',
+    'express': '^4.13.4',
+    'http-proxy-middleware': '^0.15.0'
+  }, 'apollo');
+}
