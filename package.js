e36808a4e6010bcc503627eb48e9176d37a290f8
guyAvraham <guy1812@gmail.com>
2016-10-25 16:50:01 +0300

    upgrade from apollo-server to graphql-server-express + new graphql-tools

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'apollo',
-  version: '0.1.2',
+  version: '0.1.3',
   summary: ' 🚀 Add Apollo to your Meteor app',
   git: 'https://github.com/apollostack/meteor-integration'
 });
