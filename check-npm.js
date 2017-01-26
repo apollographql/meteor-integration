7eadb9df67d3ecad07472d8a6dc2c93b895a8759
Xavier Cazalot <xavier.cazalot@gmail.com>
2017-01-26 04:57:58 +0100

    Fix #3 test runner (#63)
    Tests run again by creating a package.json with the right dependencies, so we don't have to do Npm.depends in package.js

--- check-npm.js
+++ check-npm.js
@@ -3,14 +3,14 @@ import { checkNpmVersions } from 'meteor/tmeasday:check-npm-versions';
 
 if (Meteor.isClient) {
   checkNpmVersions({
-    'apollo-client': '^0.5.0',
+    'apollo-client': '^0.7.0',
   }, 'apollo');
 } else {
   checkNpmVersions({
-    'graphql-server-express': '^0.4.3',
+    'graphql-server-express': '^0.5.0',
     "body-parser": "^1.15.2",
     "express": "^4.14.0",
     "graphql": "^0.7.0 || ^0.8.0",
-    "graphql-tools": "^0.8.0",
+    "graphql-tools": "^0.9.0",
   }, 'apollo');
 }
