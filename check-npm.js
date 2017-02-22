c2d577e0d4a60b023712bf2c23b4106681e22840
xavcz <xavier.cazalot@gmail.com>
2017-02-22 19:39:42 +0100

    add server '|| ^0.6.0' for the graphql-server-express too!

--- check-npm.js
+++ check-npm.js
@@ -8,7 +8,7 @@ if (Meteor.isClient) {
   }, 'apollo');
 } else {
   checkNpmVersions({
-    'graphql-server-express': '^0.5.0',
+    'graphql-server-express': '^0.5.0 || ^0.6.0',
     "body-parser": "^1.15.2",
     "express": "^4.14.0",
     "graphql": "^0.7.0 || ^0.8.0 || ^0.9.0",
