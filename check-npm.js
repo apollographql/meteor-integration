42f31c02e67fc6d8299a57de3211b3d6b9868c84
xavcz <xavier.cazalot@gmail.com>
2017-02-22 19:35:28 +0100

    do not force meteor/apollo users to update peerDeps while there isn't any breaking changes

--- check-npm.js
+++ check-npm.js
@@ -3,7 +3,7 @@ import { checkNpmVersions } from 'meteor/tmeasday:check-npm-versions';
 
 if (Meteor.isClient) {
   checkNpmVersions({
-    'apollo-client': '^0.8.0',
+    'apollo-client': '^0.7.0 || ^0.8.0',
     'isomorphic-fetch': '^2.2.1',
   }, 'apollo');
 } else {
@@ -11,7 +11,7 @@ if (Meteor.isClient) {
     'graphql-server-express': '^0.5.0',
     "body-parser": "^1.15.2",
     "express": "^4.14.0",
-    "graphql": "^0.9.0",
-    "graphql-tools": "^0.10.0",
+    "graphql": "^0.7.0 || ^0.8.0 || ^0.9.0",
+    "graphql-tools": "^0.9.0 || ^0.10.0",
   }, 'apollo');
 }
