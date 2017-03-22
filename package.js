63c234da1c28ebbc77d970f667a443a1812d4843
xavcz <xavier.cazalot@gmail.com>
2017-03-22 18:04:42 +0100

    publish to atmosphere

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'apollo',
-  version: '0.6.1',
+  version: '0.7.0',
   summary: ' 🚀 Add Apollo to your Meteor app',
   git: 'https://github.com/apollostack/meteor-integration',
 });
