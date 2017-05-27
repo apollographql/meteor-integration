6573a1b328ed9a40ea7f1a3959718840a383a9a5
xavcz <xavier.cazalot@gmail.com>
2017-05-27 13:46:05 +0200

    v0.9.0

--- package.js
+++ package.js
@@ -1,30 +1,30 @@
 Package.describe({
-  name: 'apollo',
-  version: '0.8.0',
-  summary: ' 🚀 Add Apollo to your Meteor app',
-  git: 'https://github.com/apollostack/meteor-integration',
+  name: "apollo",
+  version: "0.9.0",
+  summary: " 🚀 Add Apollo to your Meteor app",
+  git: "https://github.com/apollostack/meteor-integration"
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.4.2');
-  api.use(['ecmascript', 'accounts-base']);
+  api.versionsFrom("1.4.2");
+  api.use(["ecmascript", "accounts-base"]);
 
-  api.mainModule('src/main-client.js', 'client');
-  api.mainModule('src/main-server.js', 'server');
+  api.mainModule("src/main-client.js", "client");
+  api.mainModule("src/main-server.js", "server");
 });
 
 Package.onTest(function(api) {
   api.use([
-    'ecmascript',
-    'practicalmeteor:mocha',
-    'practicalmeteor:chai',
-    'practicalmeteor:mocha-console-runner',
-    'http',
-    'random',
-    'accounts-base',
-    'apollo',
+    "ecmascript",
+    "practicalmeteor:mocha",
+    "practicalmeteor:chai",
+    "practicalmeteor:mocha-console-runner",
+    "http",
+    "random",
+    "accounts-base",
+    "apollo"
   ]);
 
-  api.mainModule('tests/client.js', 'client');
-  api.mainModule('tests/server.js', 'server');
+  api.mainModule("tests/client.js", "client");
+  api.mainModule("tests/server.js", "server");
 });
